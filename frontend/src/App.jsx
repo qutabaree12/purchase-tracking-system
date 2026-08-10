46be40b824dbb46d5df2402d3f1cea3cade8e1c6
qutabaree12 <tesnimeain@gmail.com>
2026-08-10 01:58:56 +0100

    refactor: update App.jsx with RoleRoute protection for all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { ROLES } from './constants/roles'
 import MainLayout from './layouts/MainLayout'
 import Login from './pages/auth/Login'
 import AdminDashboard from './pages/admin/AdminDashboard'
@@ -11,27 +12,120 @@ import PurchaseOrderList from './pages/purchases/PurchaseOrderList'
 import PurchaseOrder from './pages/purchases/PurchaseOrder'
 import Regroupement from './pages/purchases/Regroupement'
 import Profile from './pages/Profile'
+import RoleRoute from './components/common/RoleRoute'
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          {/* ===== LOGIN ===== */}
           <Route path="/login" element={<Login />} />
+
+          {/* ===== PROTECTED ROUTES ===== */}
           <Route path="/" element={<MainLayout />}>
+            {/* Dashboard */}
             <Route index element={<AdminDashboard />} />
             <Route path="admin" element={<AdminDashboard />} />
-            <Route path="admin/users" element={<AdminUsers />} />
-            <Route path="admin/settings" element={<AdminSettings />} />
-            <Route path="purchases/requests" element={<PurchaseRequestList />} />
-            <Route path="purchases/request/new" element={<PurchaseRequest />} />
-            <Route path="purchases/request/:id" element={<PurchaseRequest />} />
-            <Route path="purchases/orders" element={<PurchaseOrderList />} />
-            <Route path="purchases/order/new" element={<PurchaseOrder />} />
-            <Route path="purchases/order/:id" element={<PurchaseOrder />} />
+
+            {/* Admin */}
+            <Route
+              path="admin/users"
+              element={
+                <RoleRoute allowedRoles={[ROLES.ADMIN]}>
+                  <AdminUsers />
+                </RoleRoute>
+              }
+            />
+            <Route
+              path="admin/settings"
+              element={
+                <RoleRoute allowedRoles={[ROLES.ADMIN]}>
+                  <AdminSettings />
+                </RoleRoute>
+              }
+            />
+
+            {/* Demandes d'achat - accessible à DEMANDEUR et ACHETEUR */}
+            <Route
+              path="purchases/requests"
+              element={
+                <RoleRoute
+                  allowedRoles={[ROLES.DEMANDEUR, ROLES.ACHETEUR]}
+                >
+                  <PurchaseRequestList />
+                </RoleRoute>
+              }
+            />
+
+            {/* Nouvelle demande - DEMANDEUR only */}
+            <Route
+              path="purchases/request/new"
+              element={
+                <RoleRoute allowedRoles={[ROLES.DEMANDEUR]}>
+                  <PurchaseRequest />
+                </RoleRoute>
+              }
+            />
+
+            {/* Consulter demande */}
+            <Route
+              path="purchases/request/:id"
+              element={
+                <RoleRoute
+                  allowedRoles={[ROLES.DEMANDEUR, ROLES.ACHETEUR]}
+                >
+                  <PurchaseRequest />
+                </RoleRoute>
+              }
+            />
+
+            {/* Bons de commande */}
+            <Route
+              path="purchases/orders"
+              element={
+                <RoleRoute
+                  allowedRoles={[ROLES.ACHETEUR, ROLES.DIRECTEUR]}
+                >
+                  <PurchaseOrderList />
+                </RoleRoute>
+              }
+            />
+
+            <Route
+              path="purchases/order/new"
+              element={
+                <RoleRoute allowedRoles={[ROLES.ACHETEUR]}>
+                  <PurchaseOrder />
+                </RoleRoute>
+              }
+            />
+
+            <Route
+              path="purchases/order/:id"
+              element={
+                <RoleRoute allowedRoles={[ROLES.ACHETEUR]}>
+                  <PurchaseOrder />
+                </RoleRoute>
+              }
+            />
+
+            {/* Regroupement - ACHETEUR only */}
+            <Route
+              path="purchases/regroupement"
+              element={
+                <RoleRoute allowedRoles={[ROLES.ACHETEUR]}>
+                  <Regroupement />
+                </RoleRoute>
+              }
+            />
+
+            {/* Profile - Tous les utilisateurs */}
             <Route path="/profile" element={<Profile />} />
-            <Route path="purchases/regroupement" element={<Regroupement />} />
           </Route>
+
+          {/* ===== CATCH-ALL ===== */}
+          <Route path="*" element={<Navigate to="/admin" replace />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
